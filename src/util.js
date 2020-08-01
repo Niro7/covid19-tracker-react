918b60d40d83e4d72dca5fce2faecc47b79ad655
K.Niroshani <niroshini.07.skk@gmail.com>
2020-08-01 22:27:59 +0530

    ADD: Map color dictionary

--- src/util.js
+++ src/util.js
@@ -1,5 +1,27 @@
-import { Table } from "@material-ui/core";
-
+import React from "react";
+// import { Table } from "@material-ui/core";
+import { numeral } from "numeral";
+import { Circle, Popup } from "leaflet";
+const casesTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    rgb: "rgb(204,16,52)",
+    half_op: "rgba(204,16,52, 0.5)",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#7DD71D",
+    rgb: "rgb(125, 215, 29)",
+    half_op: "rgba(125, 215, 29, 0.5)",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#FB4443",
+    rgb: "rgb(251, 68, 67)",
+    half_op: "rgba(251, 68, 67, 0.5)",
+    multiplier: 2000,
+  },
+};
 export const sortData = (data) => {
   const sortedData = [...data];
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
@@ -12,3 +34,11 @@ export const sortData = (data) => {
   //   });
   //   return sortedData;
 };
+
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+    ></Circle>
+  ));
