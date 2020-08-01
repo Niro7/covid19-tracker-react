bc6c3930fbf60aa2dc56aa382056838dc9d7ca98
K.Niroshani <niroshini.07.skk@gmail.com>
2020-08-01 22:27:43 +0530

    ADD: Map functionalities

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./util";
 import LineGraph from "./LineGraph";
+import numeral from "numeral";
+import "leaflet/dist/leaflet.css";
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({}); //An empty object
   const [tableData, setTableData] = useState([]); //An empty array
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
       .then((response) => response.json())
@@ -36,7 +41,9 @@ function App() {
           }));
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
+
           // console.log(data);
           // console.log(countries);
         });
@@ -61,6 +68,8 @@ function App() {
         // console.log(data);//When you select an option from dropdown you get related info
         setCountry(countryCode); //set country in input field
         setCountryInfo(data); //storecountry's data
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(3);
       });
   };
   // console.log(countryInfo);
@@ -85,21 +94,25 @@ function App() {
         <div className="app__stats">
           <InfoBox
             title="Coronavirus Cases"
-            cases={countryInfo.todayCases}
-            total={countryInfo.cases}
+            cases={numeral(countryInfo.todayCases).format("+0,0").toUpperCase()}
+            total={numeral(countryInfo.cases).format("0a").toUpperCase()}
           />
           <InfoBox
             title="Recovered"
-            cases={countryInfo.todayRecovered}
-            total={countryInfo.recovered}
+            cases={numeral(countryInfo.todayRecovered)
+              .format("+0,0")
+              .toUpperCase()}
+            total={numeral(countryInfo.recovered).format("0a").toUpperCase()}
           />
           <InfoBox
             title="Deaths"
-            cases={countryInfo.todayDeaths}
-            total={countryInfo.deaths}
+            cases={numeral(countryInfo.todayDeaths)
+              .format("+0,0")
+              .toUpperCase()}
+            total={numeral(countryInfo.deaths).format("0a").toUpperCase()}
           />
         </div>
-        <Map />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Map.css";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
-function Map() {
+function Map({ countries, zoom, center }) {
   return (
     <div className="map">
-      <h1>Map Component Created</h1>
+      <LeafletMap zoom={zoom} center={center}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LeafletMap>
     </div>
   );
 }
